349795cb5a3d95025e4a464a8d71b7224eb53ac5
EspositoLucas <espositolucas2002@gmail.com>
2024-03-12 14:14:39 -0300

    upload sql laoding scripts and project folder

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,15 +1,15 @@
-COPY company_dim
-FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/company_dim.csv'
-DELIMITER ',' CSV HEADER;
+COPY company_dim 
+FROM 'C:\Users\Public\Documents\dataset\company_dim.csv' 
+DELIMITER ',' CSV HEADER; 
 
-COPY job_postings_fact
-FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/job_postings_fact.csv'
-DELIMITER ',' CSV HEADER;
+COPY job_postings_fact 
+FROM 'C:\Users\Public\Documents\dataset\job_postings_fact.csv' 
+DELIMITER ',' CSV HEADER; 
 
-COPY skills_dim
-FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/skills_dim.csv'
-DELIMITER ',' CSV HEADER;
+COPY skills_dim 
+FROM 'C:\Users\Public\Documents\dataset\skills_dim.csv'
+DELIMITER ',' CSV HEADER; 
 
 COPY skills_job_dim
-FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/skills_job_dim.csv'
-DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\Public\Documents\dataset\skills_job_dim.csv' 
+DELIMITER ',' CSV HEADER; 
